befb078e2f4917860f53d4ff3caf01f9764eb4c4
katebennu <kate.dorrer@gmail.com>
2017-03-11 17:15:03 +0200

    add a db populator for building, apartment and grid

--- infographics/management/commands/populate.py
+++ infographics/management/commands/populate.py
@@ -2,12 +2,6 @@ from django.core.management.base import BaseCommand
 from infographics.models import Building, Apartment, Grid
 
 
-building = Building.objects.all()[0]
-total_apartments = building.total_apartments
-total_area = building.total_area
-total_inhabitants = building.total_inhabitants
-
-
 class Command(BaseCommand):
     help = 'Create apartment objects for a building'
 
@@ -16,14 +10,21 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('Successfully inserted dummy data'))
 
     def run(self):
-        building
+        building = Building.objects.create(
+            address='Fregatti',
+            total_apartments=60,
+            total_area=3000,
+            total_inhabitants=120,
+        )
 
-
+        total_apartments = building.total_apartments
+        total_area = building.total_area
+        total_inhabitants = building.total_inhabitants
 
         area = round(total_area / total_apartments)
         inhabitants = round(total_inhabitants / total_apartments)
-
         for n in range(total_apartments):
-            Apartment.objects.create(number=n+1, area=area, inhabitants=inhabitants, building=building)
+            Apartment.objects.create(number=n + 1, area=area, inhabitants=inhabitants, building=building)
 
+        Grid.objects.create(name='Suvilahti', total_units=194)
 
