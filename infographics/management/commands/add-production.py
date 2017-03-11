b3f8fd0d98a3f86d0f0bcf4ebb4823b52190957f
katebennu <kate.dorrer@gmail.com>
2017-03-11 22:01:00 +0200

    done production parser

--- infographics/management/commands/add-production.py
+++ infographics/management/commands/add-production.py
@@ -1,29 +1,31 @@
 from django.core.management.base import BaseCommand
-from infographics.models import Building, ConsumptionMeasurement
+from infographics.models import Grid, ProductionMeasurement
 import csv
 import os
-from django.utils.dateparse import parse_datetime
-from dateutil.parser import parse
 from datetime import datetime
 
 
 class Command(BaseCommand):
-    help = 'Parse and save example consumption data'
+    help = 'Parse and save example production data'
 
     def handle(self, *args, **options):
         self.run()
-        self.stdout.write(self.style.SUCCESS('Successfully inserted dummy consumption data'))
+        self.stdout.write(self.style.SUCCESS('Successfully inserted dummy production data'))
 
     def run(self):
-        building = Building.objects.all()[0]
+        grid = Grid.objects.all()[0]
 
         module_dir = os.path.dirname(os.path.abspath(__file__))
 
-        with open(os.path.join(module_dir, "fixtures", 'Fregatti_2016.csv')) as file:
-            reader = csv.reader(file)
+        with open(os.path.join(module_dir, "fixtures", 'Suvilahti_2016.csv')) as file:
+            reader = csv.reader(file, delimiter='\t')
             for row in reader:
-                _, created = ConsumptionMeasurement.objects.get_or_create(
-                    building=building,
-                    time=datetime.strptime(row[0], '%d.%m.%Y %H:%M:%S'),
-                    value=row[1]
+                try:
+                    value_per_unit = float(row[1]) / grid.total_units
+                except ValueError:
+                    value_per_unit = 0
+                _, created = ProductionMeasurement.objects.get_or_create(
+                    grid=grid,
+                    time=datetime.strptime(row[0], '%Y-%m-%dT%H:%M:%S'),
+                    value_per_unit=value_per_units
                 )
