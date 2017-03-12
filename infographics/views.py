dc0d219fdc10a7a11302137206de5690afcafce2
katebennu <kate.dorrer@gmail.com>
2017-03-12 22:33:32 +0200

    clean

--- infographics/views.py
+++ infographics/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from infographics.models import ConsumptionMeasurement, Building, Apartment
-from django.forms.models import model_to_dict
 
 
 def index(request):
@@ -8,10 +7,6 @@ def index(request):
     context['data'] = ConsumptionMeasurement.objects.filter(time__year='2016',
                                                  time__month='05',
                                                  time__day='15')
-    # context = {}
-    # for i in data:
-    #     instance = model_to_dict(i)
-    #     context.update({instance['time']: instance['value']})
 
     return render(request, "infographics/index.html", context)
 
