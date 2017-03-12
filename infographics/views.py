98b28bd83f737ef04df4abf06e29208163e0134a
katebennu <kate.dorrer@gmail.com>
2017-03-12 22:21:13 +0200

    display queryset as a list in the template

--- infographics/views.py
+++ infographics/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from infographics.models import ConsumptionMeasurement, Building, Apartment
+from django.forms.models import model_to_dict
 
 
 def index(request):
-    data = ConsumptionMeasurement.objects.filter(time__year='2016',
-                                                 time__month='05')
-    return render(request, "infographics/index.html", data)
+    context = dict()
+    context['data'] = ConsumptionMeasurement.objects.filter(time__year='2016',
+                                                 time__month='05',
+                                                 time__day='15')
+    # context = {}
+    # for i in data:
+    #     instance = model_to_dict(i)
+    #     context.update({instance['time']: instance['value']})
+
+    return render(request, "infographics/index.html", context)
 
