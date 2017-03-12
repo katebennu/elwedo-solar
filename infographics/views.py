adf476163b3e9be375a3de5c1ada5d08c8c8f442
katebennu <kate.dorrer@gmail.com>
2017-03-12 23:12:15 +0200

    update time to timestamp

--- infographics/views.py
+++ infographics/views.py
@@ -4,9 +4,8 @@ from infographics.models import ConsumptionMeasurement, Building, Apartment
 
 def index(request):
     context = dict()
-    context['data'] = ConsumptionMeasurement.objects.filter(time__year='2016',
-                                                 time__month='05',
-                                                 time__day='15')
+    context['data'] = ConsumptionMeasurement.objects.filter(timestamp__year='2016',
+                                                            timestamp__month='05',
+                                                            timestamp__day='15')
 
     return render(request, "infographics/index.html", context)
-
