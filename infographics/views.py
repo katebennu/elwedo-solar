aa49352b12dbd27f502f086b5d5264362b2050f7
katebennu <kate.dorrer@gmail.com>
2017-03-13 13:55:07 +0200

    in process

--- infographics/views.py
+++ infographics/views.py
@@ -3,9 +3,16 @@ from infographics.models import ConsumptionMeasurement, Building, Apartment
 
 
 def index(request):
+
+    data = ConsumptionMeasurement.objects.filter(timestamp__year='2016',
+                                                 timestamp__month='05',
+                                                 timestamp__day='15')
     context = dict()
-    context['data'] = ConsumptionMeasurement.objects.filter(timestamp__year='2016',
-                                                            timestamp__month='05',
-                                                            timestamp__day='15')
+    context_data = []
+
+    for i in data:
+        context_data.append({'timestamp': i.timestamp.isoformat() + 'Z', 'value': i.value })
+
+    context['context_data'] = context_data
 
     return render(request, "infographics/index.html", context)
