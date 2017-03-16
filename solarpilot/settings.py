af89fcba86d51b8c93dd83efb0f2b0a3f108fae8
katebennu <kate.dorrer@gmail.com>
2017-03-16 17:32:46 +0200

    upd

--- solarpilot/settings.py
+++ solarpilot/settings.py
@@ -134,5 +134,5 @@ STATIC_URL = '/static/'
 # # Simplified static file serving.
 # # https://warehouse.python.org/project/whitenoise/
 #
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
