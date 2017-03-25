0e83e654af6b1da361a8d726b928c06678587b56
katebennu <kate.dorrer@gmail.com>
2017-03-26 00:45:12 +0200

    update admin page

--- infographics/admin.py
+++ infographics/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from infographics.models import Building, Apartment, Grid, ConsumptionMeasurement, ProductionMeasurement
+from infographics.models import Building, Apartment, Grid, ConsumptionMeasurement, ProductionMeasurement, PanelsToInstall
 
 admin.site.register(Building)
 admin.site.register(Apartment)
 admin.site.register(Grid)
+admin.site.register(PanelsToInstall)
+admin.site.register(ConsumptionMeasurement)
+admin.site.register(ProductionMeasurement)
