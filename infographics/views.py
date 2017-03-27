311af8217b6775ab3af46dfd406f22ffd20f1cd5
katebennu <kate.dorrer@gmail.com>
2017-03-27 11:54:42 +0300

    use panels to install from db to get total production

--- infographics/views.py
+++ infographics/views.py
@@ -5,13 +5,12 @@ from django.http import JsonResponse
 
 
 def index(request):
-
-
     return render(request, "infographics/index.html")
+
 
 def timeline_update(request):
 
-    # SECOND_STAGE: replace with apartment / or building associated with ther request's user
+    # SECOND_STAGE: replace with apartment / or building associated with their request's user
     building = Building.objects.first()
 
     data = building.get_week_data()
@@ -37,7 +36,7 @@ def timeline_update(request):
 
     for i in data['production']:
         i['timestamp'] = i['timestamp'].isoformat() + 'Z'
-        i['value'] = float(i['value_per_unit']) * 200  #
+        i['value'] = float(i['value_per_unit']) * PanelsToInstall.objects.filter(use=True)[0].number_of_units  #
     # for i in data['production']:
     #     i.value = i.value_per_unit * PanelsToInstall.objects.filter(name='default')[0].number_of_units
     #     context_data['production'].append({'timestamp': i.timestamp.isoformat() + 'Z', 'value': float(i.value)})
