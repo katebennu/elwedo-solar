7a95bac9b02b2ac2edf75fe388d75411cc2d38dd
Anton Berezin <gurunars@gmail.com>
2017-03-31 20:54:43 +0300

    Reindent

--- infographics/models.py
+++ infographics/models.py
@@ -1,23 +1,23 @@
+from datetime import timedelta
+
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.db.models import Sum
 from django.db.models.functions import Trunc
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
-from datetime import datetime, timedelta
 
 
 class Apartment(models.Model):
-    def __str__(self):
-        return str(self.building.address) + ', Apartment #' + str(self.number)
-
-    number = models.fields.IntegerField(unique=True, primary_key=True, validators=[MinValueValidator(0),
-                                                                                   MaxValueValidator(9999)])
-    area = models.fields.DecimalField(max_digits=5,
-                                      decimal_places=2,
-                                      validators=[MinValueValidator(0.0),
-                                                  MaxValueValidator(999.99)])
-    inhabitants = models.fields.IntegerField(validators=[MinValueValidator(0),
-                                                         MaxValueValidator(99)])
+    number = models.fields.IntegerField(
+        unique=True,
+        primary_key=True,
+        validators=[MinValueValidator(0), MaxValueValidator(9999)])
+    area = models.fields.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0.0), MaxValueValidator(999.99)])
+    inhabitants = models.fields.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(99)])
     building = models.ForeignKey('Building')
     user = models.OneToOneField(User, related_name='apartment', default=1)
 
@@ -71,8 +71,7 @@ class Apartment(models.Model):
         consumption_by_days = q_consumption.annotate(day=Trunc('timestamp', 'day', output_field=models.DateTimeField()))
         production_by_days = q_production.annotate(day=Trunc('timestamp', 'day', output_field=models.DateTimeField()))
         # get a set of days
-        days_list = list(set([i.day for i in consumption_by_days]))
-        days_list.sort()
+        days_list = sorted(set([i.day for i in consumption_by_days]))
 
         panels = self.building.get_panels_estimate()
 
@@ -99,11 +98,11 @@ class Apartment(models.Model):
                            })
         return result
 
+    def __str__(self):
+        return str(self.building.address) + ', Apartment #' + str(self.number)
+
 
 class Building(models.Model):
-    def __str__(self):
-        return 'Building ' + str(self.address)
-
     address = models.fields.CharField(max_length=50, unique=True)
     total_apartments = models.fields.IntegerField(validators=[MinValueValidator(0),
                                                               MaxValueValidator(9999)])
@@ -192,50 +191,57 @@ class Building(models.Model):
                            })
         return result
 
+    def __str__(self):
+        return 'Building ' + str(self.address)
+
 
 class PanelsToInstall(models.Model):
+    building = models.ForeignKey(Building)
+    number_of_units = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(999999)])
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        default="default")
+    use = models.BooleanField(default=True)
+
     def __str__(self):
         return str(self.number_of_units) + ' Panels estimation, building ' + str(
             self.building.address) + ', name ' + str(self.name)
 
-    building = models.ForeignKey(Building)
-    number_of_units = models.IntegerField(validators=[MinValueValidator(0),
-                                                      MaxValueValidator(999999)])
-    name = models.CharField(max_length=100, unique=True, default="default")
-    use = models.BooleanField(default=True)
-
 
 # if one account per apartment, rewrite for one-to-one relation
 class UserMethods(User):
+
     def owns_apartment(self, apartment):
         """ Determine if user owns the apartment"""
         return self.apartments.filter(number=apartment.number).exists()
 
 
 class ConsumptionMeasurement(models.Model):
-    def __str__(self):
-        return 'Consumption on ' + str(self.timestamp)
-
     # one of the two is obligatory
     apartment = models.ForeignKey(Apartment, null=True)
     building = models.ForeignKey(Building, null=True)
     timestamp = models.DateTimeField(null=True)
-    value = models.DecimalField(max_digits=8,
-                                decimal_places=2,
-                                validators=[MinValueValidator(0.0),
-                                            MaxValueValidator(999999.99)])
+    value = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0.0), MaxValueValidator(999999.99)])
+
+    def __str__(self):
+        return 'Consumption on ' + str(self.timestamp)
 
 
 class ProductionMeasurement(models.Model):
+    timestamp = models.DateTimeField(null=True)
+    value_per_unit = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0.0), MaxValueValidator(999999.99)])
+    grid = models.ForeignKey('Grid')
+
     def __str__(self):
         return 'Production on ' + str(self.timestamp)
-
-    timestamp = models.DateTimeField(null=True)
-    value_per_unit = models.DecimalField(max_digits=8,
-                                         decimal_places=2,
-                                         validators=[MinValueValidator(0.0),
-                                                     MaxValueValidator(999999.99)])
-    grid = models.ForeignKey('Grid')
 
 
 def query_production(earliest, latest):
@@ -243,8 +249,8 @@ def query_production(earliest, latest):
 
 
 class Grid(models.Model):
-    def __str__(self):
-        return 'Grid ' + str(self.name)
-
     name = models.fields.CharField(max_length=50, unique=True)
     total_units = models.fields.IntegerField(default=200)
+
+    def __str__(self):
+        return 'Grid ' + str(self.name)
