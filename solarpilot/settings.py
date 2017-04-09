6d6e5bafa6ab8d68c24d7fe95fce33082684114d
katebennu <kate.dorrer@gmail.com>
2017-04-09 18:48:33 +0300

    whitenoise off

--- solarpilot/settings.py
+++ solarpilot/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 # # Simplified static file serving.
 # # https://warehouse.python.org/project/whitenoise/
 #
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 LOGIN_URL = 'infographics:login'
