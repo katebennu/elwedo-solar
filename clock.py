63234f5e2427c7d2c51a92499e7a3f9fd4db260f
katebennu <kate.dorrer@gmail.com>
2017-04-09 19:24:09 +0300

    utils and renaming

--- clock.py
+++ clock.py
@@ -3,14 +3,15 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'solarpilot.settings'
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management import call_command
+from infographics import utils
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=2)
 def timed_job():
-    call_command('helen-data')
-
+    utils.helen_data()
+    # call_command('helen_data')
     print('This job is run every two minutes.')
 
 sched.start()
---
+++ infographics/utils/helen_data.py
@@ -0,0 +1,79 @@
+import urllib.request
+from datetime import datetime
+from django.core.management.base import BaseCommand
+from pytz import timezone
+from infographics.models import Grid, ProductionMeasurement
+from .progress_bar import show_progress
+
+
+class Command(BaseCommand):
+    help = 'Parse and save example production data'
+
+    def handle(self, *args, **options):
+        self.run()
+        self.stdout.write(self.style.SUCCESS('Successfully updated production data'))
+
+    def run(self):
+        url = 'https://www.helen.fi/sahko/kodit/aurinkosahko/suvilahti/DownloadData/'
+        file, headers = urllib.request.urlretrieve(url)
+        contents = open(file).read()
+        rows = contents.splitlines()
+
+        grid = Grid.objects.all()[0]
+        utc = timezone('UTC')
+        total_rows = len(rows)
+        cursor = 0
+
+        for i in range(300):
+            row = rows[i]
+            row = row.split(';')
+            show_progress(cursor, total_rows)
+            if 'Arvo (kWh)' in row:
+                continue
+            try:
+                value_per_unit = float(row[1]) / grid.total_units
+            except ValueError:
+                value_per_unit = 0
+            # except IndexError:
+            #     continue
+            parse_time = datetime.strptime(row[0], '%Y-%m-%dT%H:%M:%S')
+            _, created = ProductionMeasurement.objects.get_or_create(
+                grid=grid,
+                timestamp=datetime(parse_time.year, parse_time.month, parse_time.day, parse_time.hour,
+                                   parse_time.minute, tzinfo=utc),
+                value_per_unit=float(value_per_unit)
+            )
+
+            cursor += 1
+
+
+
+
+
+
+
+
+
+        # for i in range(20):
+        #     print(i, ':', splt[i])
+
+
+
+
+
+# temp_file = open('test.txt', 'w')
+
+
+# with urllib.request.urlopen(url) as response:
+#     contents = open(response[0]).read()
+#     f = open('test.txt', 'w')
+#     f.write(contents)
+#     f.close()
+
+
+
+# response = urllib.urlretrieve(url)
+# contents = open(response[0]).read()
+# f = open('filename.ext','w')
+# f.write(contents)
+# f.close()
