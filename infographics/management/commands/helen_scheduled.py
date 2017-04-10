c81b20935beaf121febafe51701cc07d2dbf2e9a
katebennu <kate.dorrer@gmail.com>
2017-04-10 22:26:42 +0300

    60 min

--- infographics/management/commands/helen_scheduled.py
+++ infographics/management/commands/helen_scheduled.py
@@ -7,7 +7,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=5)
+
+@sched.scheduled_job('interval', minutes=60)
 def timed_job():
     print('********************* started timed_job')
 
