a5962683053aaebca37fd3ba2046cfd541f98832
katebennu <kate.dorrer@gmail.com>
2017-04-10 21:52:46 +0300

    upd

--- infographics/management/commands/helen_scheduled.py
+++ infographics/management/commands/helen_scheduled.py
@@ -6,53 +6,51 @@ from infographics.models import Grid, ProductionMeasurement
 from .progress_bar import show_progress
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+sched = BlockingScheduler()
+
 
 class Command(BaseCommand):
 
     help = 'Parse and save example production data'
 
-    sched = BlockingScheduler()
-
     def handle(self, *args, **options):
         print('********************************************************start execution')
-
-        self.sched.start()
-
+        sched.start(self)
         print('Successfully updated production data')
 
-    @sched.scheduled_job('interval', minutes=2)
-    def timed_job(self):
-        url = 'https://www.helen.fi/sahko/kodit/aurinkosahko/suvilahti/DownloadData/'
-        file, headers = urllib.request.urlretrieve(url)
-        contents = open(file).read()
-        rows = contents.splitlines()
+@sched.scheduled_job('interval', minutes=2)
+def timed_job():
+    url = 'https://www.helen.fi/sahko/kodit/aurinkosahko/suvilahti/DownloadData/'
+    file, headers = urllib.request.urlretrieve(url)
+    contents = open(file).read()
+    rows = contents.splitlines()
 
-        grid = Grid.objects.all()[0]
-        utc = timezone('UTC')
-        total_rows = len(rows)
-        cursor = 0
+    grid = Grid.objects.all()[0]
+    utc = timezone('UTC')
+    total_rows = len(rows)
+    cursor = 0
 
-        for i in range(300):
-            row = rows[i]
-            row = row.split(';')
-            show_progress(cursor, total_rows)
-            if 'Arvo (kWh)' in row:
-                continue
-            try:
-                value_per_unit = float(row[1]) / grid.total_units
-            except ValueError:
-                value_per_unit = 0
-            # except IndexError:
-            #     continue
-            parse_time = datetime.strptime(row[0], '%Y-%m-%dT%H:%M:%S')
-            _, created = ProductionMeasurement.objects.get_or_create(
-                grid=grid,
-                timestamp=datetime(parse_time.year, parse_time.month, parse_time.day, parse_time.hour,
-                                   parse_time.minute, tzinfo=utc),
-                value_per_unit=float(value_per_unit)
-            )
+    for i in range(300):
+        row = rows[i]
+        row = row.split(';')
+        show_progress(cursor, total_rows)
+        if 'Arvo (kWh)' in row:
+            continue
+        try:
+            value_per_unit = float(row[1]) / grid.total_units
+        except ValueError:
+            value_per_unit = 0
+        # except IndexError:
+        #     continue
+        parse_time = datetime.strptime(row[0], '%Y-%m-%dT%H:%M:%S')
+        _, created = ProductionMeasurement.objects.get_or_create(
+            grid=grid,
+            timestamp=datetime(parse_time.year, parse_time.month, parse_time.day, parse_time.hour,
+                               parse_time.minute, tzinfo=utc),
+            value_per_unit=float(value_per_unit)
+        )
 
-            cursor += 1
+        cursor += 1
 
 
 
