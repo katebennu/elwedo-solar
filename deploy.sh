79c5edc322e0a955adaa9da0a0588a59506529ba
katebennu <kate.dorrer@gmail.com>
2017-04-14 23:55:55 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ REMOTE_HOST=elwedo@elwedo.kbennu.com
 REMOTE_DIR=/home/elwedo/solarilot
 
 rsync -a --exclude='.git/' . ${REMOTE_HOST}:${REMOTE_DIR}
-ssh {REMOTE_HOST} "cd ${REMOTE_DIR}; docker-compose stop web; docker-compose build; docker-compose up -d --no-recreate postgres"
+ssh ${REMOTE_HOST} "cd ${REMOTE_DIR}; docker-compose stop web; docker-compose build; docker-compose up -d --no-recreate postgres"
