2d271d5e4d0bfd7a7e175b9d56e8f7d8e410e17d
katebennu <kate.dorrer@gmail.com>
2017-04-25 22:18:07 +0300

    fetch and restore ignored files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn solarpilot.wsgi --log-file -
+clock: python manage.py helen_scheduled
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+REMOTE_HOST=elwedo@elwedo.kbennu.com
+REMOTE_DIR=/home/elwedo/solarpilot
+
+rsync -a --exclude='.git/' . ${REMOTE_HOST}:${REMOTE_DIR}
+ssh ${REMOTE_HOST} "cd ${REMOTE_DIR}; docker-compose stop web; docker-compose build; docker-compose up -d --no-recreate postgres"
