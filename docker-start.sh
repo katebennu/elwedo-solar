f2b0fd0cf14d769cf0bf34d36a43ee87544f2aca
katebennu <kate.dorrer@gmail.com>
2017-04-26 13:15:21 +0300

    shorten deploy commands list

--- docker-start.sh
+++ docker-start.sh
@@ -2,9 +2,6 @@
 
 cd /package
 python manage.py migrate
-python manage.py populate
-python manage.py add-consumption
-python manage.py helen_data
 python manage.py helen_scheduled &
 
 chown -R www-data:www-data /mount
