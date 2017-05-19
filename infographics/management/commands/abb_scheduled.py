e47c31d2318e824a594a4163a4e07076397e1e58
katebennu <kate.dorrer@gmail.com>
2017-05-19 21:30:21 +0300

    produce urls for each apartment

--- infographics/management/commands/abb_scheduled.py
+++ infographics/management/commands/abb_scheduled.py
@@ -1,5 +1,7 @@
-import os, csv
 from django.core.management.base import BaseCommand
+import os, csv
+from infographics.models import Apartment, ConsumptionMeasurement
+# import requests
 
 
 class Command(BaseCommand):
@@ -11,11 +13,19 @@ class Command(BaseCommand):
 
     def run(self):
         module_dir = os.path.dirname(os.path.abspath(__file__))
+        apartments = Apartment.objects.all()
 
+        headers = {'Content-Type': 'application/xml'}
         with open(os.path.join(module_dir, "fixtures", 'auth.csv')) as file:
             reader = csv.reader(file)
             rows=list(reader)
             for row in rows:
                 auth = (row[0],row[1])
-            print(auth)
+
+        for a in apartments:
+            up = a.up_code
+            url = a.building.server_ip + '/gc=' + str(up)
+            print(url)
+            # resp = requests.get(url=url, auth=auth, verify=False)
+
 
