3db9d0fb87528a32944dafc08074e89a5c288655
katebennu <kate.dorrer@gmail.com>
2017-05-20 00:05:34 +0300

    dummy consumption for both buildings

--- infographics/management/commands/add-consumption.py
+++ infographics/management/commands/add-consumption.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('Successfully inserted dummy consumption data'))
 
     def run(self):
-        building = Building.objects.first()
+        buildings = Building.objects.all()
         apartments = Apartment.objects.all()
         utc = timezone('UTC')
         module_dir = os.path.dirname(os.path.abspath(__file__))
@@ -32,19 +32,22 @@ class Command(BaseCommand):
             for row in rows:
                 parse_time = datetime.strptime(row[0], '%d.%m.%Y %H:%M:%S')
                 try:
-                    _, created = ConsumptionMeasurement.objects.get_or_create(
-                        building=building,
-                        timestamp=datetime(parse_time.year + 1, parse_time.month, parse_time.day, parse_time.hour, parse_time.minute, tzinfo=utc),
-                        value=float(row[1])
-                    )
+                    for building in buildings:
+                        _, created = ConsumptionMeasurement.objects.get_or_create(
+                            building=building,
+                            timestamp=datetime(parse_time.year + 1, parse_time.month, parse_time.day, parse_time.hour, parse_time.minute, tzinfo=utc),
+                            value=float(row[1])
+                        )
 
                     for a in apartments:
                         _, created = ConsumptionMeasurement.objects.get_or_create(
                             apartment=a,
                             timestamp=datetime(parse_time.year + 1, parse_time.month, parse_time.day, parse_time.hour,
                                                parse_time.minute, tzinfo=utc),
-                            value=float(row[1]) / building.total_apartments
+                            value=float(row[1]) / a.building.total_area * a.area
                         )
 
                 except IntegrityError:
                     pass
+
+        # then request 2-week data from building servers
