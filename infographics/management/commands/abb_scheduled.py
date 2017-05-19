00ea7ab0d01b84199fc8ed99c2d87d4d69c10c47
katebennu <kate.dorrer@gmail.com>
2017-05-19 22:56:46 +0300

    parse xml request

--- infographics/management/commands/abb_scheduled.py
+++ infographics/management/commands/abb_scheduled.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 import os, csv
 from infographics.models import Apartment, ConsumptionMeasurement
+import xml.etree.ElementTree as ET
 import requests
 
 
@@ -15,18 +16,48 @@ class Command(BaseCommand):
         module_dir = os.path.dirname(os.path.abspath(__file__))
         apartments = Apartment.objects.all()
 
+        xml_req = """<?xml version="1.0"?>
+                <Message>
+                    <Header>
+                        <Verb>create</Verb>
+                        <Noun>MeterReadings</Noun>
+                    </Header>
+                    <Payload>
+                        <MeterReadings>
+                            <MeterReading>
+                                <Meter>
+                                    <mRID>08accb89-7ced-1af1-f235-d67dff5e8585</mRID>
+                                </Meter>
+                                <Readings>
+                                    <ReadingType ref="32.26.0.0.1.1.12.0.0.0.0.0.0.0.224.3.72.0"/>
+                                    <timePeriod>
+                                        <end>2017-05-15T09:00:00.0000000Z</end>
+                                        <start>2017-05-15T08:00:00.0000000Z</start>
+                                    </timePeriod>
+                                </Readings>
+                                <valuesInterval>
+                                    <end>2017-05-15T09:00:00.0000000Z</end>
+                                    <start>2017-05-15T08:00:00.0000000Z</start>
+                                </valuesInterval>
+                            </MeterReading>
+                        </MeterReadings>
+                    </Payload>
+                </Message>"""
+        root = ET.fromstring(xml_req)
+
         headers = {'Content-Type': 'application/xml'}
         with open(os.path.join(module_dir, "fixtures", 'auth.csv')) as file:
             reader = csv.reader(file)
-            rows=list(reader)
+            rows = list(reader)
             for row in rows:
-                auth = (row[0],row[1])
+                auth = (row[0], row[1])
 
-            for a in apartments:
-                up = a.up_code
-                url = a.building.server_ip + '/gc=' + str(up)
-                print(url)
-                resp = requests.get(url=url, auth=auth, verify=False)
-                print(resp.text)
+        for a in apartments:
+            up = a.up_code
+            url = a.building.server_ip + '/gc=' + str(up)
+            print(url)
 
+            root.find(".//mRID").text = a.mRID
 
+            resp = requests.get(url=url, auth=auth, verify=False, headers=headers)
+            print(resp.text)
