00c0888f8e4281db603598742369719673486376
katebennu <kate.dorrer@gmail.com>
2017-05-25 17:20:12 +0300

    first test runs fine

--- infographics/tests.py
+++ infographics/tests.py
@@ -15,17 +15,18 @@ class DataTestCase(TestCase):
         ConsumptionMeasurement.objects.create(timestamp=datetime(2017, 5, 21, 9),
                                               apartment=a,
                                               value=0.12)
-
         ProductionMeasurement.objects.create(timestamp=datetime(2017, 5, 21, 9),
                                              grid=g,
-                                             value=104)
+                                             percent_of_max_capacity=104/g.max_capacity)
 
     # test that production scales to building correctly
     def test_production_scales_to_building(self):
         b = Building.objects.get(name='Test Building')
+        g = ExampleGrid.objects.get(name='Test Grid')
         p = ProductionMeasurement.objects.get(timestamp=datetime(2017, 5, 21, 9),
-                                              building=b)
-        self.assertEqual(p.value, 12.24)
+                                              grid=g)
+        t = TargetCapacity.objects.get(building=b)
+        self.assertEqual(round(p.percent_of_max_capacity * t.total_capacity), round(12.24))
 
     # test that production is allocated to apartment correctly
 
