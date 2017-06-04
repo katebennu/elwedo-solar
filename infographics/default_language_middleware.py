faa6a7313af829576d5a6bbe1f61f2b310171962
Anton Berezin <gurunars@gmail.com>
2017-06-04 19:57:47 +0300

    Fixed host fetching

--- infographics/default_language_middleware.py
+++ infographics/default_language_middleware.py
@@ -1,5 +1,4 @@
 from django.utils import translation
-from django.conf import settings
 
 from django.utils.deprecation import MiddlewareMixin
 
@@ -13,7 +12,7 @@ DOMAIN_LANGUAGE_MAP = {
 class DefaultLanguageMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        site = request.META['HTTP_HOST']
+        site = request.META['HTTP_HOST'].split(":")[0]
         if "sessionid" not in request.COOKIES:
             user_language = DOMAIN_LANGUAGE_MAP.get(site, "en")
             translation.activate(user_language)
